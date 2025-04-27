f9eedb2e106809436a49a971538cc02eb7913ffc
Veicm <connormannes@gmx.de>
2025-04-27 16:09:15 +0200

    removed self.color_vault

--- rainbow_light.py
+++ rainbow_light.py
@@ -8,19 +8,15 @@ class Rainbow_Light:
         self.rainbow_light = neopixel.NeoPixel(Pin(pin), num_leds)
         self.num_leds = num_leds
 
-        self.colors_vault = [(0, 0, 0) for _ in range(num_leds)]
-
     def set_all_color(self, red, green, blue):
         '''This function will set all LEDS to the same color.
         You have to use the basic rgb color code(0-255).'''
         for i in range(self.num_leds):
             self.rainbow_light[i] = (red, green, blue)
-            self.colors_vault[i] = (red, green, blue)
         self.rainbow_light.write()
 
     def set_one_color(self, index, red, green, blue):
         self.rainbow_light[index] = (red, green, blue)
-        self.colors_vault[index] = (red, green, blue)
         self.rainbow_light.write()
 
 
@@ -32,7 +28,7 @@ class Rainbow_Light:
             b1 = from_blue
         else:
             if isinstance(mirror_index, int):
-                r1, g1, b1 = self.colors_vault[mirror_index]
+                r1, g1, b1 = self.rainbow_light[mirror_index]
             else:
                 print("Error arguments are not correct.")
         r2 = to_red
@@ -45,7 +41,6 @@ class Rainbow_Light:
             b = int(b1 + (b2 - b1) * step / steps)
             for index in range (self.num_leds):
                 self.rainbow_light[index] = (r, g, b)
-                self.colors_vault[index] = (r, g, b)
             self.rainbow_light.write()
             utime.sleep_ms(delay_ms)
 
@@ -55,7 +50,7 @@ class Rainbow_Light:
             g1 = from_green
             b1 = from_blue
         else:
-            r1, g1, b1 = self.colors_vault[index]
+            r1, g1, b1 = self.rainbow_light[index]
         r2 = to_red
         g2 = to_green
         b2 = to_blue
@@ -65,7 +60,6 @@ class Rainbow_Light:
             g = int(g1 + (g2 - g1) * step / steps)
             b = int(b1 + (b2 - b1) * step / steps)
             self.rainbow_light[index] = (r, g, b)
-            self.colors_vault[index] = (r, g, b)
             self.rainbow_light.write()
             utime.sleep_ms(delay_ms)
 
@@ -73,10 +67,8 @@ class Rainbow_Light:
     def all_off(self):
         for i in range(self.num_leds):
             self.rainbow_light[i] = (0, 0, 0)
-            self.colors_vault[i] = (0, 0, 0)
         self.rainbow_light.write()
     
     def one_off(self, index):
         self.rainbow_light[index] = (0, 0, 0)
-        self.colors_vault[index] = (0, 0, 0)
         self.rainbow_light.write()
